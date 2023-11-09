54a0a006b5e74d82a301fa049a3a86b4d10bf7c1
Dominika Trojanowska <dominika.trojanowska@s2innovation.com>
2023-11-09 18:15:35 +0100

    toggling BookShow component: title / BookEdit

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,15 +1,29 @@
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
 function BookShow({book, onDelete}) {
     
-    const handleClick = () => {
+    const [showEdit, setShowEdit] = useState(false)
+
+    const handleEditClick = () => {
+        setShowEdit(!showEdit)
+    }
+
+    const handleDeleteClick = () => {
         console.log("deleting ", book.id, book.title)
         onDelete(book.id)
     }
 
-    return <div className="book-show">
-        {book.title}
-        <div className="actions">
-            <button className="delete" onClick={handleClick}>Delete</button>
+    let content = <h3>{book.title}</h3> 
+    if (showEdit){  //  === true
+        content = <BookEdit/>
+    }
 
+    return <div className="book-show">
+        <div>{content}</div>
+        <div className="actions">
+            <button className="edit" onClick={handleEditClick}>Edit</button>
+            <button className="delete" onClick={handleDeleteClick}>Delete</button>  
         </div>
         </div>;
 }
